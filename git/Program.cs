a1e2f88117d434c0fb41c302c851a3defee14654
37533 <37533@LAPTOP-GAM2DSJ2>
2023-04-08 23:44:23 +0300

    вывод на консоль человечика

--- git/Program.cs
+++ git/Program.cs
@@ -6,7 +6,11 @@ namespace git
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("     =====   ");
+            Console.WriteLine("   (| o o |) ");
+            Console.WriteLine("    |  ^  |  ");
+            Console.WriteLine("    | '-' |  ");
+            Console.WriteLine("    +-----+  ");
         }
     }
 }
