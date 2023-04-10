29d34ffc02b8585d2d283e7203581abe436ce3c5
37533 <37533@LAPTOP-GAM2DSJ2>
2023-04-10 11:19:23 +0300

    человечик новая причёска

--- git/Program.cs
+++ git/Program.cs
@@ -6,7 +6,7 @@ namespace git
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("     =====   ");
+            Console.WriteLine("     "+'"'+'"'+'"'+'"'+'"'+'"'+"   ");
             Console.WriteLine("   (| o o |) ");
             Console.WriteLine("    |  ^  |  ");
             Console.WriteLine("    | '-' |  ");
